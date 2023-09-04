a647795c96cf44a47fd1fc01389659cbe930e080
Himanshu Yadav <56100874+KratoSkills@users.noreply.github.com>
2023-09-04 13:22:25 +0530

    Create 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -8,19 +8,3 @@ class Solution:
                 return [num_map[complement], i]
             num_map[num] = i
         return []
-
-# Using 2 pointer approach
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums = [(num, i) for i, num in enumerate(nums)]
-        nums.sort()
-        left, right = 0, len(nums) - 1
-        while left < right:
-            current_sum = nums[left][0] + nums[right][0]
-            if current_sum == target:
-                return [nums[left][1], nums[right][1]]
-            elif current_sum < target:
-                left += 1
-            else:
-                right -= 1
-        return []
